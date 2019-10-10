214af5781868e2f393ce96365616610bfdc40ec6
绪文 祁 <qixuwen@qq.com>
2019-10-10 11:24:26 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿using Emgu.CV;
+using Emgu.CV.UI;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DemoForEmgu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            bool flag = disposing && this.components != null;
+            if (flag)
+            {
+                this.components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new Container();
+            this.camara = new ImageBox();
+            this.picture = new ImageBox();
+            this.TakePhoto = new Button();
+            this.textBox1 = new TextBox();
+            this.button1 = new Button();
+            this.button2 = new Button();
+            this.panel1 = new Panel();
+            this.label1 = new Label();
+            this.imageBox1 = new ImageBox();
+            this.button3 = new Button();
+            ((ISupportInitialize)this.camara).BeginInit();
+            ((ISupportInitialize)this.picture).BeginInit();
+            this.panel1.SuspendLayout();
+            ((ISupportInitialize)this.imageBox1).BeginInit();
+            base.SuspendLayout();
+            this.camara.Location = new Point(12, 12);
+            this.camara.Name = "camara";
+            this.camara.Size = new Size(433, 426);
+            this.camara.SizeMode = PictureBoxSizeMode.Zoom;
+            this.camara.TabIndex = 2;
+            this.camara.TabStop = false;
+            this.picture.Location = new Point(481, 13);
+            this.picture.Name = "picture";
+            this.picture.Size = new Size(307, 160);
+            this.picture.SizeMode = PictureBoxSizeMode.Zoom;
+            this.picture.TabIndex = 2;
+            this.picture.TabStop = false;
+            this.TakePhoto.Location = new Point(713, 179);
+            this.TakePhoto.Name = "TakePhoto";
+            this.TakePhoto.Size = new Size(75, 23);
+            this.TakePhoto.TabIndex = 3;
+            this.TakePhoto.Text = "拍照";
+            this.TakePhoto.UseVisualStyleBackColor = true;
+            this.TakePhoto.Click += this.TakePhoto_Click;
+            this.textBox1.Location = new Point(481, 179);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new Size(213, 21);
+            this.textBox1.TabIndex = 4;
+            this.button1.Location = new Point(713, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "训练";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += this.Button1_Click;
+            this.button2.Location = new Point(713, 261);
+            this.button2.Name = "button2";
+            this.button2.Size = new Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "识别";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += this.Button2_Click;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new Point(481, 302);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new Size(307, 136);
+            this.panel1.TabIndex = 7;
+            this.label1.AutoSize = true;
+            this.label1.Location = new Point(24, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new Size(0, 12);
+            this.label1.TabIndex = 0;
+            this.imageBox1.Location = new Point(807, 13);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new Size(433, 426);
+            this.imageBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            this.imageBox1.TabIndex = 8;
+            this.imageBox1.TabStop = false;
+            this.button3.Location = new Point(608, 221);
+            this.button3.Name = "button3";
+            this.button3.Size = new Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "加载图片";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += this.Button3_Click;
+            base.AutoScaleDimensions = new SizeF(6f, 12f);
+            base.AutoScaleMode = AutoScaleMode.Font;
+            base.ClientSize = new Size(1294, 468);
+            base.Controls.Add(this.button3);
+            base.Controls.Add(this.imageBox1);
+            base.Controls.Add(this.panel1);
+            base.Controls.Add(this.button2);
+            base.Controls.Add(this.button1);
+            base.Controls.Add(this.textBox1);
+            base.Controls.Add(this.TakePhoto);
+            base.Controls.Add(this.picture);
+            base.Controls.Add(this.camara);
+            base.Name = "Form1";
+            this.Text = "Form1";
+            ((ISupportInitialize)this.camara).EndInit();
+            ((ISupportInitialize)this.picture).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((ISupportInitialize)this.imageBox1).EndInit();
+            base.ResumeLayout(false);
+            base.PerformLayout();
+        }
+
+        // Token: 0x04000008 RID: 8
+        private ImageBox camara;
+
+        // Token: 0x04000009 RID: 9
+        private ImageBox picture;
+
+        // Token: 0x0400000A RID: 10
+        private Button TakePhoto;
+
+        // Token: 0x0400000B RID: 11
+        private TextBox textBox1;
+
+        // Token: 0x0400000C RID: 12
+        private Button button1;
+
+        // Token: 0x0400000D RID: 13
+        private Button button2;
+
+        // Token: 0x0400000E RID: 14
+        private Panel panel1;
+
+        // Token: 0x0400000F RID: 15
+        private Label label1;
+
+        // Token: 0x04000010 RID: 16
+        private ImageBox imageBox1;
+
+        // Token: 0x04000011 RID: 17
+        private Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Face;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+
+namespace DemoForEmgu
+{
+
+    partial class Form1 : Form
+    {
+        private ICapture capture;
+
+        private CascadeClassifier cascadeClassifierForEye;
+
+
+        private CascadeClassifier cascadeClassifierForFace;
+
+        private FaceRecognizer faceRecognizer;
+        public Form1()
+        {
+            this.InitializeComponent();
+            this.faceRecognizer = new EigenFaceRecognizer(80, double.PositiveInfinity);
+            this.faceRecognizer.Read("face.xml");
+            string eyeFile = "haarcascades/haarcascade_eye.xml";
+            this.cascadeClassifierForEye = new CascadeClassifier(eyeFile);
+            string faceFile = "haarcascades/haarcascade_frontalface_default.xml";
+            this.cascadeClassifierForFace = new CascadeClassifier(faceFile);
+            CvInvoke.UseOpenCL = true;
+            this.capture = new VideoCapture(0, VideoCapture.API.Any);
+            Task.Run(delegate ()
+            {
+                while (true)
+                {
+                    using (Image<Bgr, byte> frameImage = this.capture.QueryFrame().ToImage<Bgr, byte>(false))
+                    {
+                        this.camara.Image = this.DetectFace(frameImage);
+                        Thread.Sleep(100);
+                    }
+                }
+            });
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+        }
+
+        private Image<Bgr, byte> DetectFace(Image<Bgr, byte> frameImage)
+        {
+            bool flag = frameImage != null;
+            if (flag)
+            {
+                Image<Gray, byte> grayImage = frameImage.Convert<Gray, byte>();
+                Rectangle[] eyes = this.cascadeClassifierForEye.DetectMultiScale(grayImage, 1.1, 12, Size.Empty, default(Size));
+                foreach (Rectangle eye in eyes)
+                {
+                    frameImage.GrabCut(eye, 1);
+                    frameImage.Draw(eye, new Bgr(Color.Red), 3, LineType.EightConnected, 0);
+                }
+                Rectangle[] faces = this.cascadeClassifierForFace.DetectMultiScale(grayImage, 1.1, 12, Size.Empty, default(Size));
+                Rectangle[] array2 = faces;
+                for (int j = 0; j < array2.Length; j++)
+                {
+                    Rectangle face = array2[j];
+                    Image<Gray, byte> face2 = grayImage.GetSubRect(face);
+                    frameImage.GrabCut(face, 1);
+                    frameImage.Draw(face, new Bgr(Color.Red), 3, LineType.EightConnected, 0);
+                    face2._EqualizeHist();
+                    FaceRecognizer.PredictionResult result = this.faceRecognizer.Predict(face2.Resize(100, 100, Inter.Cubic));
+                    Console.WriteLine(result.Label);
+                    string path = Directory.GetDirectories("img")[result.Label];
+                    base.Invoke(new Action(delegate ()
+                    {
+                        this.label1.Text = string.Format("当前用户是:{0},相似度：{1}", path.Split(new char[]
+                        {
+                            '\\'
+                        }).Last<string>(), result.Distance);
+                    }));
+                    string txt = string.Format("{0},{1}", path.Split(new char[]
+                    {
+                        '\\'
+                    }).Last<string>(), result.Distance);
+                    Font font = new Font("宋体", 60f, GraphicsUnit.Pixel);
+                    SolidBrush fontLine = new SolidBrush(Color.Blue);
+                 
+                    float xPos = (float)(face.X + (face.Width / 2 - txt.Length * 14 / 2));
+                    float yPos = (float)(face.Y - 21);
+                    frameImage.Draw(txt, new Point((int)xPos, (int)yPos), FontFace.HersheyComplex, 1.0, new Bgr(Color.AntiqueWhite), 1, LineType.AntiAlias);
+                }
+            }
+            return frameImage;
+        }
+
+        private void TakePhoto_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (Image<Gray, byte> faceToSave = new Image<Gray, byte>(this.capture.QueryFrame().Bitmap))
+                {
+                    Rectangle[] faces = this.cascadeClassifierForFace.DetectMultiScale(faceToSave, 1.1, 12, Size.Empty, default(Size));
+                    bool flag = faces.Length != 1;
+                    if (!flag)
+                    {
+                        Image<Gray, byte> face = faceToSave.GetSubRect(faces[0]);
+                        string username = this.textBox1.Text.Trim();
+                        DirectoryInfo dir = new DirectoryInfo("img/" + username);
+                        bool flag2 = !dir.Exists;
+                        if (flag2)
+                        {
+                            dir.Create();
+                        }
+                        string fileName = dir.GetFiles().Length.ToString();
+                        string filePath = string.Format("img/{0}/{1}.bmp", username, fileName);
+                        face._EqualizeHist();
+                        face.ToBitmap().Save(filePath);
+                        this.picture.Image = face;
+                        this.Button1_Click(null, null);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            List<Mat> faceImages = new List<Mat>();
+            List<int> faceLabels = new List<int>();
+            int i = 0;
+            foreach (string dirPath in Directory.GetDirectories("img"))
+            {
+                foreach (string filePath in Directory.GetFiles(dirPath))
+                {
+                    FileInfo file = new FileInfo(filePath);
+                    FileStream memory = file.OpenRead();
+                    Image<Gray, byte> faceImage = new Image<Gray, byte>(new Bitmap(memory));
+                    faceImages.Add(faceImage.Resize(100, 100, Inter.Cubic).Mat);
+                    faceLabels.Add(i);
+                }
+                i++;
+            }
+            this.faceRecognizer.Train(faceImages.ToArray(), faceLabels.ToArray());
+            this.faceRecognizer.Write("face.xml");
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            using (Image<Gray, byte> frameImage = new Image<Gray, byte>(this.capture.QueryFrame().Bitmap))
+            {
+                bool flag = frameImage != null;
+                if (flag)
+                {
+                    Rectangle[] faces = this.cascadeClassifierForFace.DetectMultiScale(frameImage, 1.1, 12, Size.Empty, default(Size));
+                    bool flag2 = faces.Length != 1;
+                    if (flag2)
+                    {
+                        this.label1.Text = "未检出人脸";
+                    }
+                    else
+                    {
+                        Image<Gray, byte> face = frameImage.GetSubRect(faces[0]);
+                        this.faceRecognizer.Read("face.xml");
+                        face._EqualizeHist();
+                        FaceRecognizer.PredictionResult result = this.faceRecognizer.Predict(face.Resize(100, 100, Inter.Cubic));
+                        Console.WriteLine(result.Label);
+                        string path = Directory.GetDirectories("img")[result.Label];
+                        this.label1.Text = string.Format("当前用户是:{0},差异度：{1}", path.Split(new char[]
+                        {
+                            '\\'
+                        }).Last<string>(), result.Distance);
+                    }
+                }
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "jpg|*.jpg|png|*.png|bmp|*.bmp";
+            bool flag = fileDialog.ShowDialog() == DialogResult.OK;
+            if (flag)
+            {
+                Image<Bgr, byte> modelImage = new Image<Bgr, byte>(fileDialog.FileName);
+                modelImage = this.DetectFace(modelImage);
+                this.imageBox1.Height = modelImage.Height;
+                this.imageBox1.Width = modelImage.Width;
+                this.imageBox1.Image = modelImage;
+            }
+        }
+
+      
+    }
+}
